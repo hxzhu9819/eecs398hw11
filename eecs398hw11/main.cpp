36a22870f48855ae7c4e61ba3a9da56b1bb8ee19
hxzhu <hxzhu@umich.edu>
2018-11-21 12:57:23 -0500

    Initial Commit

---
+++ eecs398hw11/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  eecs398hw11
+//
+//  Created by 朱皓轩 on 11/21/18.
+//  Copyright © 2018 Zhu Haoxuan. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
